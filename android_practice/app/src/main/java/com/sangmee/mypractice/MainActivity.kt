4e4de0e6c9b02dfd2e4a43b911185d1b2cd1ee2e
sangminkim <apfhdznzl@gmail.com>
2021-05-08 20:15:10 +0900

    #2 RecyclerView - DiffUtil

--- android_practice/app/src/main/java/com/sangmee/mypractice/MainActivity.kt
+++ android_practice/app/src/main/java/com/sangmee/mypractice/MainActivity.kt
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .flatMap {
-                adapter.setPosts(it)
+                adapter.submitList(it)
                 Observable.fromIterable(it)
                     .subscribeOn(Schedulers.io())
             }
--- android_practice/app/src/main/java/com/sangmee/mypractice/RecyclerAdapter.kt
+++ android_practice/app/src/main/java/com/sangmee/mypractice/RecyclerAdapter.kt
@@ -3,13 +3,14 @@ package com.sangmee.mypractice
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sangmee.mypractice.databinding.LayoutPostListItemBinding
 import com.sangmee.rxjavapractice.models.Post
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
 
-    private var posts = ArrayList<Post>()
+    private var posts = mutableListOf<Post>()
     private lateinit var binding: LayoutPostListItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
@@ -25,17 +26,39 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>
         holder.bind(posts[position])
     }
 
-    fun setPosts(posts: List<Post>) {
-        this.posts.clear()
-        this.posts.addAll(posts)
-        notifyDataSetChanged()
+    fun submitList(newPosts: List<Post>) {
+        val oldList = posts
+        val diffResult: DiffUtil.DiffResult =
+            DiffUtil.calculateDiff(RecyclerDiffUtil(oldList, newPosts))
+        posts.clear()
+        posts.addAll(newPosts)
+        diffResult.dispatchUpdatesTo(this)
     }
 
     fun updatePost(post: Post) {
-        with(posts.indexOf(post)) {
-            posts[this] = post
-            notifyItemChanged(this)
+        val oldList = posts
+        val newList = posts as ArrayList
+        with(newList.indexOf(post)) {
+            newList[this] = post
         }
+        val diffResult: DiffUtil.DiffResult =
+            DiffUtil.calculateDiff(RecyclerDiffUtil(oldList, newList))
+        this.posts = newList
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class RecyclerDiffUtil(private val oldList: List<Post>, private val newList: List<Post>) :
+        DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldList[oldItemPosition].id == newList[newItemPosition].id
+
+        override fun getOldListSize() = oldList.size
+
+
+        override fun getNewListSize() = newList.size
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldList[oldItemPosition] == newList[newItemPosition]
     }
 
     class RecyclerViewHolder(private val binding: LayoutPostListItemBinding) :
