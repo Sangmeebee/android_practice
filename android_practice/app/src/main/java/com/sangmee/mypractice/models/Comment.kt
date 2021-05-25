1c7ab7b4cd89ce86f525f36e05e9406f08fc0e58
sangminkim <apfhdznzl@gmail.com>
2021-05-25 17:36:15 +0900

    #6 create entity

--- android_practice/app/src/main/java/com/sangmee/mypractice/models/Comment.kt
+++ android_practice/app/src/main/java/com/sangmee/mypractice/models/Comment.kt
@@ -1,13 +1,17 @@
 package com.sangmee.mypractice.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "comment_table")
 @Parcelize
 data class Comment(
+    @PrimaryKey
+    val id: Int,
     val body: String,
     val email: String,
-    val id: Int,
     val name: String,
     val postId: Int
 ) : Parcelable
--- android_practice/app/src/main/java/com/sangmee/mypractice/models/Post.kt
+++ android_practice/app/src/main/java/com/sangmee/mypractice/models/Post.kt
@@ -1,12 +1,17 @@
 package com.sangmee.mypractice.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity(tableName = "post_table")
 @Parcelize
 data class Post(
-    val body: String,
+    @PrimaryKey
     val id: Int,
+    val body: String,
     val title: String,
     val userId: Int,
     var comments: List<Comment>?
